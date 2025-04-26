324347defa96cb481a3144f5e92473772ea98b95
felnek1t <felnek1t@mail.ru>
2025-04-26 16:20:11 +0300

    Add files via upload

--- Order.java
+++ Order.java
@@ -2,16 +2,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-    private int tableNUMBER;
+    private int tableNumber;
     private List<Dish> dishes;
 
     public Order(int tableNumber) {
-        this.tableNUMBER = tableNumber;
+        this.tableNumber = tableNumber;
         this.dishes = new ArrayList<>();
     }
 
-    public void addDish(Dish dish) {
+    public boolean addDish(Dish dish) {
+        // Проверка на дубликаты
+        for (Dish existingDish : dishes) {
+            if (existingDish.getName().equalsIgnoreCase(dish.getName())) {
+                System.out.println("Dish \"" + dish.getName() + "\" already exists in the order.");
+                return false;
+            }
+        }
         dishes.add(dish);
+        System.out.println("Added dish: " + dish);
+        return true;
+    }
+
+    public boolean removeDish(String dishName) {
+        for (Dish dish : dishes) {
+            if (dish.getName().equalsIgnoreCase(dishName)) {
+                dishes.remove(dish);
+                System.out.println("Removed dish: " + dish);
+                return true;
+            }
+        }
+        System.out.println("Dish \"" + dishName + "\" not found in the order.");
+        return false;
     }
 
     public double calculateTotal() {
@@ -23,7 +44,7 @@ public class Order {
     }
 
     public int getTableNumber() {
-        return tableNUMBER;
+        return tableNumber;
     }
 
     public List<Dish> getDishes() {
